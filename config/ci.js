ea9f111e32eaf933b4aca24a09e725b494ae39be
Timothy Teoh <timothy.teoh@frogasia.com>
2018-10-04 18:44:54 +0800

    Travis changes and other stuff

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://root:example1@ds251632.mlab.com:51632/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:7017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
---
+++ tests/setup.js
@@ -0,0 +1,9 @@
+jest.setTimeout(20000)
+require('../models/User')
+require('../models/Blog');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
